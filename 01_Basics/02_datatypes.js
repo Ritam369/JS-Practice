e44ac38a8aea2b9e35798a149c77b92c146e59eb
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-01-29 07:57:26 +0000

    Datatypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Treat all js codes as newer version
+
+//alert(3+3) //we are using node.js, not browser
+//Code readability should be high
+
+console.log(3+3)
+
+console.log("Ritam");
+
+let name = "Ritam"
+let age = 19
+let isLoggesdIn = true
+
+//number
+//bigint
+//string => ""
+//boolean
+//undefined => a variable is declared but value is not assigned
+//null => Standalone value (representation of empty value) doesnot means zero!!
+//symbol => unique
+//These are primitive datatypes
+
+console.log(typeof "Ritam");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
