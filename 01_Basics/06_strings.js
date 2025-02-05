9d759eace96c8cee5d08fcd03c3879955a629538
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-02-05 07:48:22 +0000

    Nums and maths ongoing

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -18,4 +18,17 @@ console.log(newName.toUpperCase());
 console.log(newName.charAt(3)); //character at index
 console.log(newName.indexOf('t')); //index of value
 console.log(newName.substring(0,3)); //Ending index er aage obdhi print hbe
-// console.log(newName.slice(-9,2));
+// If we use negative undex at strt then it completely ignore that index and will start from 0 index
+// console.log(newName.slice(-8,2)); //Works same as substring but here we can use negative index
+
+const newString = "   Ritam" 
+console.log(newString);
+console.log(newString.trim()); //Kono word er aage o pore thaka extra space ke delte kore deyy...but 2to word er majhkhaner space ke as it is rakhe 
+
+console.log(newName.replace('a','y')); //Single character replace kore
+
+console.log(newName.includes('it')); //Give keyword or characters gulo ache kina check kore
+
+console.log(newName.split('_')); //The string will be splitted based on the given separator in the parameter and it will be converted into an array
+
+
---
+++ 01_Basics/07_nums_and_maths.js
@@ -0,0 +1,9 @@
+const score = 500
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance); //Specially mentioned in output as a number
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3)); //point er por jotogulo ghor chaii parameter e setaii pass korbo
+
