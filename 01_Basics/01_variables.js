8ed6ffc795ba6d0b9857a11cd15987996bd78e9c
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-04-05 19:23:13 +0000

    Variables updated

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -7,8 +7,10 @@ let accountState //As here is no value assigned.. thus it will print "undefined"
 // accountID = 234115 //Not allowed 
 /*
     Prefer not to use var
-    because of block scope and functional scope
+    because of issue in block scope and functional scope
     always prefer to use let and const
+
+    Variables declared with var are function-scoped, meaning they're accessible throughout the entire function, even before they're declared due to hoisting. This can lead to unexpected behavior and bugs, especially in larger programs. In contrast, let and const are block-scoped, providing better control over variable visibility.
 */
 
 accountPassword = "23165"
