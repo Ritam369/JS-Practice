2a22a5e2ed9b4a40853fb83f625fda0080e2ab26
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-04-05 19:54:52 +0000

    Datatypes updated

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -7,16 +7,17 @@ console.log(3+3)
 
 console.log("Ritam");
 
-let name = "Ritam"
-let age = 19
-let isLoggesdIn = true
+let name = "Ritam" //string
+let age = 19 //number
+let isLoggesdIn = true //boolean
 
 //number
 //bigint
 //string => ""
 //boolean
-//undefined => a variable is declared but value is not assigned
-//null => Standalone value (representation of empty value) doesnot means zero!!
+//undefined => This is the default value given to something that hasn't been assigned any value yet. It's like an empty placeholder.
+//null => Standalone value (representation of empty value) doesnot means zero!! This is a value you explicitly assign to indicate "no value" or "nothing."
+// undefined is its own type (primitive), while null is an object type.
 //symbol => unique
 //These are primitive datatypes
 
