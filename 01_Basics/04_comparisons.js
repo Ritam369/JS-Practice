dcf0831ec3e58e6a4461b3dce3fc26a7c61c42d2
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-04-07 15:20:43 +0000

    Comparisons updated

--- 01_Basics/04_comparisons.js
+++ 01_Basics/04_comparisons.js
@@ -1,12 +1,12 @@
-console.log("2" > 1); //Conversion happens by default in ==
-console.log("02" > 1);
+console.log("2" > 1); //true
+console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
 /*
     The reason is that an equality check == and comparisons >, <, <=, >= work differently
-    Comparisons convert null to a number, treating ir as 0
+    Comparisons convert null to a number, treating it as 0
     Thus, null >= 0 is true but null == 0 false
  */
 
