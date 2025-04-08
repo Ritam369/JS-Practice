fa3a4b6bc6a25dfc69fd40753cb7ecabb6370184
Ritam Saha <149296403+Ritam369@users.noreply.github.com>
2025-04-08 18:39:31 +0000

    Summary updated

--- 01_Basics/Datatypes_Summary.js
+++ 01_Basics/Datatypes_Summary.js
@@ -1,5 +1,10 @@
+//Datatypes are divided into two parts based on how data is stored in memory and how it is accesed. These two types are-->
+// (a) Primitive data-type
+// (b) Non-primitive/Reference data-type
+
+
 // Primitive (Call by Value)
-//7 types--> String, Number, Boolean, null(empty), undefined, symblo(to make a value unique), BigInt, 
+//7 types--> String, Number, Boolean, null(empty), undefined, symbol(to make a value unique), bigint
 
 
 /*JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
@@ -7,23 +12,25 @@
 For example, in JavaScript, you can do the following:
 
 let x = 10; // x is now a number
+let x = 10.3; // x is now a number
 x = "Hello"; // x is now a string
 x = true; // x is now a boolean
 On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
 
-Languages like Java, C++, and TypeScript are statically typed, and they require you to specify the variable type explicitly when declaring them:
+Languages like Java, C, C++, and TypeScript are statically typed, and they require you to specify the variable type explicitly when declaring them:
 
 int x = 10; // x is a variable of type int
 String name = "John"; // name is a variable of type String
+
 JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime errors if not handled carefully. Static typing, on the other hand, provides better type safety at the cost of some initial verbosity and strictness.*/
 
 //Symblol
 const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(typeof id);
-
 console.log(id === anotherId);
-const bigNumber = 264167465896548764687n //bigInt
+
+const bigNumber = 264167465896548764687n //bigint //seshe n add kore dile seta bigint hoye jacche
 console.log(bigNumber);
 console.log( typeof bigNumber);
 
@@ -43,6 +50,6 @@ let myObj ={
 const myFunction = function(){  //function is declared in this way
     console.log("Hello World");
 }
-console.log(typeof myFunction); //o/p => object function
+console.log(typeof myFunction); //o/p => function (object)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
