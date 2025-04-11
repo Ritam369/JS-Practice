3e455abfdafd43948412f9a52fb603b64f369d48
ritamsaha75579 <ritamsaha75579@gmail.com>
2025-04-11 18:42:06 +0000

    Array complete

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -14,11 +14,23 @@ const dc = ["superman", "batman", "flash"]
 
 
 // spread operator(...) (better & alternative method than concat())
-const mightyHeroes = [...marvel,...dc]
+const mightyHeroes = [...marvel,...dc] //this is better because it can merge more than two arrays within one new array
 console.log(mightyHeroes);
 
-// flat method
 
+// flat method
 const arr = [1, 2, 3, [4, 5, 6], 5, 6, [8, 4, [true, false]]]
-const newArray = arr.flat(Infinity)
+const newArray = arr.flat(2) //It will spread the subarrays within an given array upto the level of given depth
 console.log(newArray);
+//If you are not sure about the depth then just give Infinity then it will take upto the ultimate level
+
+// Array.isArray() and Array.from() methods
+console.log(Array.isArray("Ritam")); //checks whether the passed value is an array or not
+console.log(Array.from("Ritam")); //converts the passed value into an array
+console.log(Array.from([1, 2, 3], (x) => x + x)); //map function
+
+const score1 = 100
+const score2 = 200
+const score3 = 300
+
+console.log(Array.of(score1, score2, score3))
