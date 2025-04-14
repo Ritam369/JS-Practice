c7fd14f3acdad5954dbc103d60e9c8c75ad6c11f
ritamsaha75579 <ritamsaha75579@gmail.com>
2025-04-14 07:46:12 +0000

    Function done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,7 +15,7 @@ function sayMyName() {
 
 //Another way to declare function
 const addTwoNumbers = function(num1,num2) { //function definition e pass hoyy parameters
-    console.log(num1+num2);
+    // console.log(num1+num2);
 }
 
 // addTwoNumbers(2,3) //function calling e pass hoyy arguments
@@ -24,14 +24,14 @@ const addTwoNumbers = function(num1,num2) { //function definition e pass hoyy pa
 // addTwoNumbers(2,null)
 
 const result = addTwoNumbers(4, 6)
-console.log("Result :",result); //udefined asbe because function toh kichu return korche nah
+// console.log("Result :",result); //udefined asbe because function toh kichu return korche nah
 
 const addThreeNumbers = function(num1,num2,num3) {
     // let res = num1+num2+num3
     return num1+num2+num3 //return er por jaii lagabo seta execute hbe nah karon seta unreachable hbe karon taar aaagei return keyword use hoye geche
 }
 // const res = addThreeNumbers(3, 4, 6)
-console.log("Result :",addThreeNumbers(3, 4, 6));
+// console.log("Result :",addThreeNumbers(3, 4, 6));
 
 
 function userloginMessage(name = "User"){ //jodi kichu pass nah kora hoyy tahole by default ei value ta jabe and jodi kichu pass hoyy argument diye then seta ekhaner value taake overwrite kore debe
@@ -40,6 +40,47 @@ function userloginMessage(name = "User"){ //jodi kichu pass nah kora hoyy tahole
     }
     return `${name} just logged in!!`
 }
-console.log(userloginMessage("Ritam"));
-console.log(userloginMessage("")); //JS e if er condition e jodi ("") erom pass korano hoyy tahole seta false bojhay
-console.log(userloginMessage());
+// console.log(userloginMessage("Ritam"));
+// console.log(userloginMessage("")); //JS e if er condition e jodi ("") erom pass korano hoyy tahole seta false bojhay
+// console.log(userloginMessage());
+
+
+//Use od rest operator (denoted by ...) (same denotion as spread operator but on application basis they are different operation)
+// By rest operator I can paas any number of arguments it won't hamper the definition of the function
+//rest operator returns an array
+
+function totalCartPrice(...num){
+    return num
+}
+
+// function totalCartPrice(val1,val2, ...num){
+//     return num
+// }
+
+console.log(totalCartPrice(1000))
+console.log(totalCartPrice(100, 230, 33000, 4500))
+
+//How to pass objects as the paraameters of a function??
+
+const userDetails = {
+    name : "Ritam",
+    age : 21
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.name} and Age is ${anyObject.age}`);
+}
+
+handleObject(userDetails)
+handleObject({
+    name : "George",
+    age : 14
+})
+
+//Passing array as paarameter of function
+const myArray = [10, 20, 30]
+function returnThirdValue(getArray){
+    return getArray[2]
+}
+console.log(returnThirdValue(myArray));
+console.log(returnThirdValue([250, 360, 470]));
