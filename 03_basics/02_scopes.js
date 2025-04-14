716c6d05f720ad85da268df97ca4a00becfd76ca
ritamsaha75579 <ritamsaha75579@gmail.com>
2025-04-14 11:23:08 +0000

    Nested-scope done and basic hoisting done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,3 +18,42 @@ console.log(c);
 //let and const are block-scope means if the variables are declared within an scope then
 //it will only work within that scope only, outside the scope it will give ReferenceError
 //But!! var does not follow this rull, it doesn't follow the block-scope rule
+
+function one() {
+    const userName = "Ritam"
+
+    function two() {
+        const website = "https://ritam-saha.vercel.app/"
+        console.log(userName);
+    }
+    // console.log(website); //This will show an error as "ReferenceError: website is not defined"
+    two()
+}
+one();
+
+if (true) {
+    const firstName = "Ritam"
+    if(firstName === "Ritam"){
+        const lastName = " Saha"
+        console.log(firstName + lastName);
+    }
+    // console.log(lastName); //This will show an error as "ReferenceError: lastName is not defined"
+}
+// console.log(firstName); //This will show an error as "ReferenceError: firstName is not defined"
+
+
+// +++++++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++
+
+
+console.log(addTwo(5)) //o/p --> 7
+function addTwo(num){
+    return num+2
+}
+// console.log(addTwo(5)) //o/p --> 7
+
+// console.log(addOne(3)) //ReferenceError: Cannot access 'addOne' before initialization
+//This problem asised because here the function is stored in the variable addOne, thus we can't acccess this expression before initialization
+const addOne = function(num) { //Anther way to make function. This functionn is stored within a variable, this is known as expression
+    return num+1
+}
+console.log(addOne(3)) //o/p-->4
